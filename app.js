900a9b0454b7fc559062c7c647285d4bb372a6af
ahmedhamada7 <ahmedhamdakamel4545@gmail.com>
2025-05-02 15:28:46 +0300

    Fix syntax error in app.js

--- app.js
+++ app.js
@@ -47,5 +47,5 @@ app.post("/", upload.single("file"), (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http:
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
